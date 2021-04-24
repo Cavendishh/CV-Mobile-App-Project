9e640b993a50bce5a55eb0b81ced7972b23b3ce6
jannekavander-workfellow <jannekavander@workfellow.ai>
2021-04-24 12:36:55 +0300

    Contact frame done

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -8,7 +8,23 @@ import {
   IonGrid,
   IonRow,
   IonCol,
+  IonItem,
+  IonList,
+  IonCardContent,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonButton,
+  IonIcon,
 } from '@ionic/react'
+import {
+  logoGithub,
+  logoLinkedin,
+  logoStackoverflow,
+  mailOutline,
+  locationOutline,
+} from 'ionicons/icons'
 import './Contact.css'
 import Menu from '../../components/Menu/Menu'
 import MenuButton from '../../components/Menu/MenuButton'
@@ -26,7 +42,84 @@ const Contact = () => {
         </IonHeader>
         <IonContent>
           <IonGrid>
-            <IonText>. . .</IonText>
+            <IonCard>
+              <IonCardHeader>
+                <IonCardSubtitle className='cardYear'>
+                  Contact Details
+                </IonCardSubtitle>
+                <IonCardTitle className='cardTitle'>
+                  Janne Kavander
+                </IonCardTitle>
+                <IonCardSubtitle className='cardSub'>
+                  In case you want to contact me I would recommend emailing me
+                  for the fastest response. My email can be copied below by
+                  clicking 'Copy' button. Do not be afraid to contact me.
+                </IonCardSubtitle>
+              </IonCardHeader>
+              <IonCardContent>
+                <IonList>
+                  <IonItem>
+                    <IonIcon src={locationOutline} />
+                    Finland, Southern
+                  </IonItem>
+                  <IonItem>
+                    <IonIcon src={mailOutline} />
+                    kavanderjanne@gmail.com
+                    <IonButton
+                      fill='clear'
+                      id='kavanderjanne@gmail.com'
+                      onClick={() => {
+                        navigator.clipboard.writeText('kavanderjanne@gmail.com')
+                      }}
+                    >
+                      Copy
+                    </IonButton>
+                  </IonItem>
+
+                  <IonItem>
+                    <IonButton
+                      fill='clear'
+                      onClick={() =>
+                        window.open('https://github.com/Cavendishh', '_blank')
+                      }
+                    >
+                      <IonIcon src={logoGithub} />
+                      Github
+                    </IonButton>
+                  </IonItem>
+
+                  <IonItem>
+                    <IonButton
+                      fill='clear'
+                      onClick={() =>
+                        window.open(
+                          'https://www.linkedin.com/in/janne-kavander/',
+                          '_blank'
+                        )
+                      }
+                    >
+                      <IonIcon src={logoLinkedin} />
+                      Linkedin
+                    </IonButton>
+                  </IonItem>
+
+                  <IonItem>
+                    <IonButton
+                      fill='clear'
+                      onClick={() =>
+                        window.open(
+                          'https://stackoverflow.com/users/13715738/janne?tab=profile',
+                          '_blank'
+                        )
+                      }
+                    >
+                      <IonIcon src={logoStackoverflow} />
+                      Stack Overflow
+                    </IonButton>
+                  </IonItem>
+                </IonList>
+              </IonCardContent>
+            </IonCard>
           </IonGrid>
         </IonContent>
       </IonPage>
